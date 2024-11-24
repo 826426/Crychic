764913f7b8e9801150171de72c52f6875855d499
826426 <151946040+826426@users.noreply.github.com>
2024-11-24 17:21:06 +0800

    Jump

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,17 +1,52 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class Test: MonoBehaviour
 {
     public float maxMoveSpeed;
     public Vector3 velocity;
-    public float horizontal;
+    public float h;
+    public float v;
     public Rigidbody2D rb;
     public float moveH;
     public float introDir;
     public int jumpFrame;
     public Vector2 moveSpeed;
     public float jumpForce;
+    public float climbSpeed;
+    public GameObject target1;
+    public GameObject target2;
+    public GameObject target3;
+    public Vector2 dashDir;
+    public float vertivDir;
+    public float jumpSpeed;
+    public bool isCanFall;
+    public bool isCanControl;
+    public float sp = 120;
+    public bool IsClimb
+    {
+        get
+        {
+            return isCanClimb && Input.GetKey(KeyCode.LeftControl);
+        }
+    }
+
+    public bool JumpKeyDown
+    {
+        get
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                return true;
+            }
+            else if (jumpFrame > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
     public bool IsOnGround
     {
         get
@@ -19,17 +54,26 @@ public class Test: MonoBehaviour
             return downBox.collider != null ? true : false;
         }
     }
-    public bool IsCanClimb
-    {
-        get
-        {
-            return leftBox.Length > 0 || rightBox.Length > 0;
-        }
-    }
+    public bool isCanClimb;
     public int playerLayer;
     public RaycastHit2D downBox;
     public RaycastHit2D[] leftBox;
     public RaycastHit2D[] rightBox;
+    public RaycastHit2D[] HorizontalBox
+    {
+        get
+        {
+            if(leftBox.Length > 0)
+            {
+                return leftBox;
+            }
+            else if(rightBox.Length > 0)
+            {
+                return rightBox;
+            }
+            return null;
+        }
+    }
     private void Start()
     {
 
@@ -40,10 +84,63 @@ public class Test: MonoBehaviour
 
     private void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-        downBox = Physics2D.BoxCast(transform.position - new Vector3(0,0.4f,0), new Vector2(1, 0.25f), 0, Vector2.down, 0.05f, playerLayer);
-        leftBox = Physics2D.BoxCastAll(transform.position - new Vector3(0, 0.4f, 0), new Vector2(1, 1.2f), 0, Vector2.left, 0.05f, playerLayer);
-        rightBox= Physics2D.BoxCastAll(transform.position - new Vector3(0, 0.4f, 0), new Vector2(1, 1.2f), 0, Vector2.right, 0.05f, playerLayer);
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+        if (IsOnGround && v < 0)
+        {
+            vertivDir = 0;
+        }
+        else
+        {
+            vertivDir = v;
+        }
+        dashDir = new Vector2(h, vertivDir).normalized;
+        if(dashDir == Vector2.zero)
+        {
+            dashDir = Vector2.zero;//后面更换为角色面朝方向
+        }
+        downBox = Physics2D.BoxCast(transform.position, new Vector2(0.5f, 0.8f), 0, Vector2.down, 0.1f, playerLayer);
+        leftBox = Physics2D.BoxCastAll(transform.position, new Vector2(0.5f, 0.8f), 0, Vector2.left, 0.3f, playerLayer);
+        rightBox= Physics2D.BoxCastAll(transform.position, new Vector2(0.5f, 0.8f), 0, Vector2.right, 0.3f, playerLayer);
+        if (!IsClimb)
+        {
+            sp = 120;
+        }
+        if (HorizontalBox != null && HorizontalBox.Length > 0)
+        {
+            Debug.DrawLine(transform.position, HorizontalBox[0].point, Color.red);
+        }
+        if (leftBox.Length > 0 || rightBox.Length > 0)
+        {
+            isCanClimb = true;
+        }
+        else
+        {
+            isCanClimb = false;
+        }
+        if (IsClimb)
+        {
+            velocity.x = 0;
+            if(v > 0 && transform.position.y - HorizontalBox[0].point.y > 0.9f)
+            {
+                StartCoroutine("ClambAutoJump");
+                return;
+            }
+            if(v <= 0 && transform.position.y - HorizontalBox[0].point.y > 0.7f || !Input.GetKey(KeyCode.LeftControl) || sp <= 0)
+            {
+                velocity.y -= climbSpeed * Time.deltaTime;
+            }
+            else if(transform.position.y - HorizontalBox[0].point.y <= 0.7f || Input.GetKey(KeyCode.LeftControl))
+            {
+                velocity.y = v * climbSpeed;
+            }
+            if (JumpKeyDown)
+            {
+                velocity.y += jumpForce;
+                jumpFrame = 2;
+                sp -= 30f;
+            }
+        }
         if (IsOnGround)
         {
             velocity.y = 0;
@@ -52,39 +149,43 @@ public class Test: MonoBehaviour
         {
             jumpFrame = 2;
         }
-        if (JumpKeyDown() && IsOnGround)
+        if (JumpKeyDown && IsOnGround)
         {
             velocity.y += jumpForce;
             jumpFrame = 2;
         }
-        if (Input.GetKeyDown(KeyCode.A) && horizontal >= 0)
+        if(JumpKeyDown && isCanClimb)
         {
-            horizontal = -1;
+            //空中靠近墙壁，会有个小蹬墙跳
+        }
+        if (Input.GetKeyDown(KeyCode.A) && h >= 0)
+        {
+            h = -1;
         }
         else if (Input.GetKeyUp(KeyCode.A))
         {
             if (Input.GetKey(KeyCode.D))
             {
-                horizontal = 1;
+                h = 1;
             }
             else
             {
-                horizontal = 0;
+                h = 0;
             }
         }
-        if (Input.GetKeyDown(KeyCode.D) && horizontal <= 0)
+        if (Input.GetKeyDown(KeyCode.D) && h <= 0)
         {
-            horizontal = 1;
+            h = 1;
         }
         else if (Input.GetKeyUp(KeyCode.D))
         {
             if (Input.GetKey(KeyCode.A))
             {
-                horizontal = -1;
+                h = -1;
             }
             else
             {
-                horizontal = 0;
+                h = 0;
             }
         }
     }
@@ -94,14 +195,21 @@ public class Test: MonoBehaviour
         {
             jumpFrame--;
         }
+        //冲刺状态
+
+        //爬墙状态
+        if (IsClimb)
+        {
+            sp -= 1f;
+        }
         //下落状态
-        if (!IsOnGround)
+        if (!IsOnGround && !IsClimb)
         {
             velocity.y -= 150f * Time.deltaTime;
             velocity.y = Mathf.Clamp(velocity.y, -25, velocity.y);
         }
         //减速状态
-        if (horizontal == 0 || (horizontal > 0 && velocity.x < 0) || (horizontal < 0 && velocity.x > 0))
+        if (h == 0 || (h > 0 && velocity.x < 0) || (h < 0 && velocity.x > 0))
         {
             introDir = velocity.x > 0 ? 1 : -1;
             moveH = Mathf.Abs(velocity.x);
@@ -120,7 +228,7 @@ public class Test: MonoBehaviour
             velocity.x = moveH * introDir;
         }
         //加速状态
-        if (horizontal > 0)
+        if (h > 0)
         {
             if (IsOnGround)
             {
@@ -135,7 +243,7 @@ public class Test: MonoBehaviour
                 velocity.x = maxMoveSpeed;
             }
         }
-        else if (horizontal < 0)
+        else if (h < 0)
         {
             if (IsOnGround)
             {
@@ -152,20 +260,22 @@ public class Test: MonoBehaviour
         }
         rb.MovePosition(transform.position + velocity * Time.fixedDeltaTime);
     }
-    public bool JumpKeyDown()
+    IEnumerator ClambAutoJump()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        var posY = Mathf.Ceil(transform.position.y);
+        velocity = Vector2.zero;
+        while(posY + 1f > transform.position.y)
         {
-            return true;
+            velocity.y = jumpSpeed;
+            velocity.x = h * 5f;
+            yield return null;
         }
-        else if(jumpFrame > 0)
-        {
-            return true;
-        }
-        return false;
+        velocity = Vector2.zero;
     }
     private void OnDrawGizmos()
     {
-        Gizmos.DrawWireCube(transform.position - new Vector3(0, 0.4f, 0), new Vector2(1, 0.25f));
+        Gizmos.DrawWireCube(target1.transform.position, new Vector2(0.5f, 0.8f));
+        Gizmos.DrawWireCube(target2.transform.position, new Vector2(0.5f, 0.8f));
+        Gizmos.DrawWireCube(target3.transform.position, new Vector2(0.5f, 0.8f));
     }
 }
