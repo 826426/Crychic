30231da584d9994ed517fd2fb3fb409fb743bc3f
826426 <151946040+826426@users.noreply.github.com>
2024-11-27 17:40:09 +0800

    Complete

---
+++ Assets/Script/PlayerParam.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+[CreateAssetMenu(fileName = "PlayerParams", menuName = "ScriptableObject/Player Param", order = 1)]
+public class PlayerParam: ScriptableObject
+{
+    [Header("水平方向参数")]
+    public int maxSpeed;
+
+}
---
+++ Assets/Script/State.cs
@@ -0,0 +1,51 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class State
+{
+    protected Player player;
+    protected StateMachine stateMachine;
+    private string animBoolName;
+    public State(Player player,StateMachine stateMachine, string animBoolName)
+    {
+        this.player = player;
+        this.stateMachine = stateMachine;
+        this.animBoolName = animBoolName;
+    }
+    public virtual void Enter()
+    {
+        player.anim.SetBool(animBoolName, true);
+    }
+    public virtual void Exit()
+    {
+        player.anim.SetBool(animBoolName, false);
+    }
+    public virtual void Update()
+    {
+        player.anim.SetFloat("Hor", Mathf.Abs(player.velocity.x));
+        player.anim.SetFloat("Vel", player.velocity.y);
+        player.anim.SetFloat("ClVel", Mathf.Abs(player.velocity.y));
+        if (player.input.DashKeyDown && player.dashCount > 0)
+        {
+            stateMachine.ChangeState(player.dashState);
+            return;
+        }
+        if (player.input.JumpKeyDown && player.IsOnGround && player.curState != CurState.Jump && player.curState != CurState.Dash)
+        {
+            player.Jump();
+        }
+        if(player.curState != CurState.Jump && !player.IsOnGround && player.CheckIsSlide() && player.curState != CurState.Climb && player.curState != CurState.Slide && player.curState != CurState.Dash)
+        {
+            stateMachine.ChangeState(player.slideState);
+        }
+        if(player.input.ClimbKey && player.IsCanClimb && player.curState != CurState.Jump && player.curState != CurState.Dash && player.curState != CurState.Climb)
+        {
+            stateMachine.ChangeState(player.climbState);
+        }
+    }
+    public virtual void FixedUpdate()
+    {
+
+    }
+ 
+}
