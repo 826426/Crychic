db7664f8bb715e9630cb8599e09d0e3f71d80ac0
SPY1111111 <1248463827@qq.com>
2024-11-29 22:03:33 +0800

    跳板

---
+++ Assets/Scripts/JumpingPlatform.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class JumpingPlatform : MonoBehaviour
+{
+    [Label("速度加成")] 
+    public Vector2 vel;
+    [Label("最大横向速度")]
+    public float maxVelX;
+
+    private bool isPlayerOnPlatform = false;
+
+    private void Update()
+    {
+        if (isPlayerOnPlatform)
+        {
+            TriggerPlayerJump();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerOnPlatform = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerOnPlatform = false;
+        }
+    }
+
+    private void TriggerPlayerJump()
+    {
+        var player = FindObjectOfType<Player>();
+
+        if (player != null)
+        {
+            player.Jump(vel,new Vector2(maxVelX,0));
+        }
+    }
+}
